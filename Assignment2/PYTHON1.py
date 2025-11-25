dae024625d20f6f1d5b03c7a046e6969b4d304b4
Sonam Yadav <sonamraoyadav2509@gmail.com>
2025-11-25 22:40:29 +0530

    Add files via upload

---
+++ Assignment2/PYTHON1.py
@@ -0,0 +1,29 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=================== RESTART: C:/Users/SONAM/Desktop/PYTHON.py ==================
+Grade Book Analyzer !!!!
+Enter number of students: 5
+Enter name of student 1: SONAM YADAV
+Enter marks of SONAM YADAV: 99
+Enter name of student 2: PRACHI MANWAL
+Enter marks of PRACHI MANWAL: 98
+Enter name of student 3: DEV KAUSHIK
+Enter marks of DEV KAUSHIK: 92
+Enter name of student 4: KESHAV
+Enter marks of KESHAV: 89
+Enter name of student 5: HARSHVARDHAN
+Enter marks of HARSHVARDHAN: 87
+
+--- Student Report ---
+Name: SONAM YADAV | Marks: 99 | Grade: A
+Name: PRACHI MANWAL | Marks: 98 | Grade: A
+Name: DEV KAUSHIK | Marks: 92 | Grade: A
+Name: KESHAV | Marks: 89 | Grade: B
+Name: HARSHVARDHAN | Marks: 87 | Grade: B
+
+--- Overall Analysis ---
+Total Marks of Class: 465
+Average Marks of Class: 93.0
+Highest Marks: 99 by SONAM YADAV
+Lowest Marks: 87 by HARSHVARDHAN
